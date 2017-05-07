7fe71233b0dbc1f5a1bdd7e52137538a0b13ba2f
Michael Davidsaver <mdavidsaver@gmail.com>
2017-05-07 19:30:40 -0400

    exclude testFull from testharness for <3.15

--- testApp/src/symbTestHarness.c
+++ testApp/src/symbTestHarness.c
@@ -1,9 +1,17 @@
 #include "epicsUnitTest.h"
+#include "epicsVersion.h"
 #include "epicsExit.h"
+
+#ifndef VERSION_INT
+#  define VERSION_INT(V,R,M,P) ( ((V)<<24) | ((R)<<16) | ((M)<<8) | (P))
+#  define EPICS_VERSION_INT VERSION_INT(EPICS_VERSION, EPICS_REVISION, EPICS_MODIFICATION, EPICS_PATCH_LEVEL)
+#endif
 
 int testParse(void);
 int testInit(void);
+#if EPICS_VERSION_INT>=VERSION_INT(3,15,0,0)
 int testFull(void);
+#endif
 
 void symbTestHarness(void)
 {
@@ -11,7 +19,9 @@ void symbTestHarness(void)
 
     runTest(testParse);
     runTest(testInit);
+#if EPICS_VERSION_INT>=VERSION_INT(3,15,0,0)
     runTest(testFull);
+#endif
 
     epicsExit(0);
 }
