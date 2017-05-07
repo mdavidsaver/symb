315f9c8eeb43a65eec44e5029dd34d7a687fe237
Michael Davidsaver <mdavidsaver@gmail.com>
2017-05-07 18:34:32 -0400

    export all test symbols
    Base 3.14 epicsFindSymbol() doesn't work on Linux

---
+++ testApp/src/testFull.syms.c
@@ -0,0 +1,6 @@
+#include <devSymbTable.h>
+SYMTAB_EXTERN(test_double)
+SYMTAB_START()
+SYMTAB_ENTRY(test_double)
+SYMTAB_END()
+
--- testApp/src/testInit.syms.c
+++ testApp/src/testInit.syms.c
@@ -1,6 +1,10 @@
 #include <devSymbTable.h>
+SYMTAB_EXTERN(foo)
 SYMTAB_EXTERN(bar)
+SYMTAB_EXTERN(baz)
 SYMTAB_START()
-//SYMTAB_ENTRY(bar)
+SYMTAB_ENTRY(foo)
+SYMTAB_ENTRY(bar)
 SYMTAB_ENTRY_ALIAS(bar, "bar_alias")
+SYMTAB_ENTRY(baz)
 SYMTAB_END()
